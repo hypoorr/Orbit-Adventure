7dd2c2c2bb876e2b8d2ae7a8ba21908caa6a3f4b
hypoorr <ollie.heron@gmail.com>
2026-03-12 16:34:10 +0000

    inventory progress

--- Orbit Adventure/Assets/Scripts/Inventory.cs
+++ Orbit Adventure/Assets/Scripts/Inventory.cs
@@ -11,12 +11,23 @@ public class InventoryItem // inventory item layout
 
 public class Inventory : MonoBehaviour
 {
+    public GameObject itemElement;
     public static List<InventoryItem> items = new List<InventoryItem>(); // list to hold all items
+
+
     private bool inventoryOpen = false;
+
     [SerializeField] private GameObject inventoryUI;
+    [SerializeField] private Transform inventoryBG;
+    private List<GameObject> itemElementList = new List<GameObject>();
+    private int elementIndex = 0;
+
+
     void Start()
     {
-        items.Add(new InventoryItem {itemName = "Stone", itemQuantity = 1}); // adds an item "Stone" to inventory with 1 quantity
+        items.Add(new InventoryItem {itemName = "Stone", itemQuantity = 1});
+        items.Add(new InventoryItem {itemName = "Fuel (L)", itemQuantity = 5});
+        items.Add(new InventoryItem {itemName = "Gold", itemQuantity = 99}); // adds an item "Stone" to inventory with 1 quantity
         Debug.Log(items[0].itemName); // will log "Stone"
     }
 
@@ -24,6 +35,7 @@ public class Inventory : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.I))
         {
+            UpdateInventoryDisplay();
             if (inventoryOpen) // if inventory already open, close it and hide cursor
             {
                 inventoryOpen = false;
@@ -41,11 +53,29 @@ public class Inventory : MonoBehaviour
 
         }
 
-        if (inventoryOpen)
+
+        //copy inventory item element
+        //locate item name text and set itemname on it
+        //locate item quantity text and set itemquantity on it
+
+
+        void UpdateInventoryDisplay()
         {
-            //copy inventory item element
-            //locate item name text and set itemname on it
-            //locate item quantity text and set itemquantity on it
+            elementIndex = 0;
+            foreach(GameObject i in itemElementList)
+            {
+                Destroy(i);
+            }
+            foreach(InventoryItem i in items)
+            {
+                Debug.Log(i.itemName + " " + i.itemQuantity);
+                GameObject newElement = Instantiate(itemElement,inventoryBG, false);
+                // new Vector3(20 * elementIndex, 0, 0), Quaternion.identity, 
+                newElement.transform.position = new Vector3(20 * elementIndex, 0, 0);
+                itemElementList.Add(newElement);
+                elementIndex += 1;
+                //Debug.Log(items[i].itemName, items[i].itemQuantity);
+            }
         }
     }
 
