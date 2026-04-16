0c9eee83d9f695e114e0a9f7459b32fc77e7adff
hypoorr <ollie.heron@gmail.com>
2026-04-16 16:55:26 +0100

    beginning of notifications implement

---
+++ Orbit Adventure/Assets/Scripts/Input/Notifications.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Notifications : MonoBehaviour
+{
+    public GameObject notificationTemplate;
+
+    static private void NewNotification(string title, string text)
+    {
+        GameObject newElement = Instantiate(notificationTemplate, GameObject, true);
+        
+        // create notification in corner
+        // replace titletext with title
+        // replace notitext with text
+        // disappear after 5 seconds
+    }
+}
