2f4760721623f046ad02ed48bf96c6b24c6aa75a
hypoorr <ollie.heron@gmail.com>
2026-04-28 13:09:50 +0100

    menu screen ship animation

--- Orbit Adventure/Assets/Scripts/Menu/MenuScreen.cs
+++ Orbit Adventure/Assets/Scripts/Menu/MenuScreen.cs
@@ -1,9 +1,25 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using DG.Tweening;
 public class MenuScreen : MonoBehaviour
 {
+    public GameObject ship;
+
     public void Play()
     {
+        ship.GetComponent<MenuShipMove>().enabled = false;
+        StartCoroutine(AnimateShip());
+    }
+
+    IEnumerator AnimateShip()
+    {
+        ship.transform.DORotate(ship.transform.eulerAngles + new Vector3(-45, 0, 0), 2f)
+            .SetEase(Ease.InOutSine);
+        yield return new WaitForSeconds(2f);
+        ship.transform.DOMove(new Vector3(-30, -35, 0), 1f)
+            .SetEase(Ease.InOutCubic);
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene("InsideShip");
     }
 }
