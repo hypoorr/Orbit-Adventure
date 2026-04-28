c8ddd0968d665c75cf4989cb397d5db448750bd1
hypoorr <ollie.heron@gmail.com>
2026-04-28 13:21:54 +0100

    menu animation value adjustments

--- Orbit Adventure/Assets/Scripts/Menu/MenuScreen.cs
+++ Orbit Adventure/Assets/Scripts/Menu/MenuScreen.cs
@@ -14,12 +14,12 @@ public class MenuScreen : MonoBehaviour
 
     IEnumerator AnimateShip()
     {
-        ship.transform.DORotate(ship.transform.eulerAngles + new Vector3(-45, 0, 0), 2f)
+        ship.transform.DORotate(ship.transform.eulerAngles + new Vector3(-35, 0, 0), 2f)
             .SetEase(Ease.InOutSine);
         yield return new WaitForSeconds(2f);
         ship.transform.DOMove(new Vector3(-30, -35, 0), 1f)
             .SetEase(Ease.InOutCubic);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.8f);
         SceneManager.LoadScene("InsideShip");
     }
 }
