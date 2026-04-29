052855acb9661433c266d122c44cee4f5ffb0424
hypoorr <ollie.heron@gmail.com>
2026-04-29 13:52:53 +0100

    fade out black image on menu

--- Orbit Adventure/Assets/Scripts/Menu/MenuScreen.cs
+++ Orbit Adventure/Assets/Scripts/Menu/MenuScreen.cs
@@ -2,24 +2,47 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using DG.Tweening;
+using UnityEngine.UI;
 public class MenuScreen : MonoBehaviour
 {
     public GameObject ship;
+    public ParticleSystem particle1;
+    public ParticleSystem particle2;
+    public GameObject fadeUI;
 
     public void Play()
     {
         ship.GetComponent<MenuShipMove>().enabled = false;
+        gameObject.GetComponent<Button>().enabled = false;
         StartCoroutine(AnimateShip());
     }
 
     IEnumerator AnimateShip()
     {
+        particle1.Stop();
+        particle2.Stop();
+        yield return new WaitForSeconds(1f);
         ship.transform.DORotate(ship.transform.eulerAngles + new Vector3(-35, 0, 0), 2f)
             .SetEase(Ease.InOutSine);
         yield return new WaitForSeconds(2f);
+        particle1.Play();
+        particle2.Play();
         ship.transform.DOMove(new Vector3(-30, -35, 0), 1f)
             .SetEase(Ease.InOutCubic);
-        yield return new WaitForSeconds(0.8f);
+        StartCoroutine(StartFade());
+        yield return new WaitForSeconds(1f);
+    }
+
+    IEnumerator StartFade()
+    {
+        fadeUI.SetActive(true);
+        Color color = fadeUI.GetComponent<Image>().color;
+        for (int i = 0; i <= 255; i++)
+        {
+            fadeUI.GetComponent<Image>().color = new Color(0f, 0f, 0f, i * 0.01f);
+            yield return new WaitForSeconds(0.01f);
+        }
         SceneManager.LoadScene("InsideShip");
+
     }
 }
